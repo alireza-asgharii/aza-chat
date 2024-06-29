048f4f3b7f6cd99fd5b6ca6d160e91344261799c
Alireza Asghari <aa9967316@gmail.com>
2024-06-29 11:19:29 +0330

    🔧 Update tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        primaryBlue: "#5463FF",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
