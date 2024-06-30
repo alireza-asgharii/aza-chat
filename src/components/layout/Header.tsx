2b5d12f0c69024c2064dc2bc6793ef9b18430fbe
Alireza Asghari <aa9967316@gmail.com>
2024-06-30 12:23:40 +0330

    🛂 Manage user access level in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,61 @@
 import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
+
 import { Button } from "../ui/button";
 import { ModeToggle } from "../ui/ModeToggle";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import SignOutButton from "../module/SignOutButton";
 
-const Header = () => {
+const Header = async () => {
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.getUser();
+
+  console.log(data);
+
   return (
     <header className="flex justify-between items-center py-2 border-b-[1px]">
       <div>
-        <Link className="font-bold p-2 relative" href='/'>
-        AZA Chat
-        <span className="absolute bg-yellow-300 dark:bg-primaryBlue  top-1/2 left-1/2 w-full h-3 -z-10 translate-x-[-50%] translate-y-[-20%]"></span>
+        <Link className="font-bold p-2 relative" href="/">
+          AZA Chat
+          <span className="absolute bg-yellow-300 dark:bg-primaryBlue  top-1/2 left-1/2 w-full h-3 -z-10 translate-x-[-50%] translate-y-[-20%]"></span>
         </Link>
       </div>
       <div className="flex items-center">
-        <Button className="mr-3">Sign In</Button>
+        {!data?.user && (
+          <Link href="/signin">
+            <Button className="mr-3">Sign In</Button>
+          </Link>
+        )}
+
+        {data?.user && (
+          <>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar className="mr-3">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>
+                    {data.user.email ? data.user.email[0].toUpperCase() : "P"}
+                  </AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <SignOutButton>Sign Out</SignOutButton>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
+        )}
+
         <ModeToggle />
       </div>
     </header>
