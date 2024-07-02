0dc459c2ae1de7f5ac08e8a6bed6ac64446881fe
Alireza Asghari <aa9967316@gmail.com>
2024-07-02 12:29:00 +0330

    ✨ Initialization for user state

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,52 +13,54 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import SignOutButton from "../module/SignOutButton";
+import InitUser from "@/lib/store/InitUser";
 
 const Header = async () => {
   const supabase = createClient();
   const { data, error } = await supabase.auth.getUser();
 
-  console.log(data);
-
   return (
-    <header className="flex justify-between items-center py-2 border-b-[1px]">
-      <div>
-        <Link className="font-bold p-2 relative" href="/">
-          AZA Chat
-          <span className="absolute bg-yellow-300 dark:bg-primaryBlue  top-1/2 left-1/2 w-full h-3 -z-10 translate-x-[-50%] translate-y-[-20%]"></span>
-        </Link>
-      </div>
-      <div className="flex items-center">
-        {!data?.user && (
-          <Link href="/signin">
-            <Button className="mr-3">Sign In</Button>
+    <>
+    <InitUser user={data?.user ?? undefined} />
+      <header className="flex justify-between items-center py-2 border-b-[1px]">
+        <div>
+          <Link className="font-bold p-2 relative" href="/">
+            AZA Chat
+            <span className="absolute bg-yellow-300 dark:bg-primaryBlue  top-1/2 left-1/2 w-full h-3 -z-10 translate-x-[-50%] translate-y-[-20%]"></span>
           </Link>
-        )}
+        </div>
+        <div className="flex items-center">
+          {!data?.user && (
+            <Link href="/signin">
+              <Button className="mr-3">Sign In</Button>
+            </Link>
+          )}
 
-        {data?.user && (
-          <>
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Avatar className="mr-3">
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>
-                    {data.user.email ? data.user.email[0].toUpperCase() : "P"}
-                  </AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <SignOutButton>Sign Out</SignOutButton>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </>
-        )}
+          {data?.user && (
+            <>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Avatar className="mr-3">
+                    <AvatarImage src={data.user?.user_metadata?.avatar_url ?? ""} />
+                    <AvatarFallback>
+                      {data.user.email ? data.user.email[0].toUpperCase() : "P"}
+                    </AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <SignOutButton>Sign Out</SignOutButton>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          )}
 
-        <ModeToggle />
-      </div>
-    </header>
+          <ModeToggle />
+        </div>
+      </header>
+    </>
   );
 };
 
