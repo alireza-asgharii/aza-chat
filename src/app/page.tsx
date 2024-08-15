a4af4126b5d29a8a66c1db79415c1d3bb0efbc76
Alireza Asghari <aa9967316@gmail.com>
2024-08-15 11:58:55 +0330

    💄 Change some style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Message from "@/components/module/Message";
 
 export default function Home() {
   return (
-    <div className="h-[90vh] flex justify-between flex-col px-1">
+    <div className="h-[90vh] flex justify-between flex-col px-1 relative">
       <ChatMessages />
       <ChatInput />
     </div>
